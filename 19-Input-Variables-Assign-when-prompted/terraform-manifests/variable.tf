b757f0128f796c5e48c77883ef1bc0c5130941fc
aa <aa@gmail.com>
2025-07-09 11:51:47 +0200

    19th file first commit

---
+++ 19-Input-Variables-Assign-when-prompted/terraform-manifests/variable.tf
@@ -0,0 +1,28 @@
+variable "business_unit" {
+  type        = string
+  description = "business unit"
+  #default     = "hr"
+}
+
+variable "enviornment" {
+  type        = string
+  description = "project environment"
+  #default     = "dev"
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "rg name"
+  #default     = "myrg1"
+}
+
+variable "location_name" {
+  type        = string
+  description = "location name"
+  #default     = "East US"
+}
+
+variable "virtual_network" {
+  type        = string
+  #description = "vnet name"
+}
